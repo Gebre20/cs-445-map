8fae3219095c687f47d253c336227bb6e0792990
Gebrehiwot Tekulu <gebrehiwot.tek@gmail.com>
2021-11-04 21:16:54 -0500

    Lab6 Added

--- lab6/isPrime.js
+++ lab6/isPrime.js
@@ -1,10 +1,12 @@
-const isPrime = num => new Promise((resolve, reject) => {
-    setTimeout(function() {
-        for (let i = 2, s = Math.sqrt(num); i <= s; i++)
-            if (num % i === 0) reject({ prime: false });
-        resolve({ prime: true});
-    }, 500);
-});
+async function isPrime(num){
+    for (let i = 2, s = Math.sqrt(num); i <= s; i++){
+        if (num % i === 0){
+            return `{prime: false}`;
+        } 
+    }
+    return `{prime: true}`;    
+}
+
 async function isPrimeAsync() {
     let result = await isPrime(7);
     console.log(result);
